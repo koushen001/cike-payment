7a301bf72370c9f4ffd2adcdb1785a808ca51a23
cike <875569847@qq.com>
2017-09-06 16:27:41 +0800

    Initial commit

---
+++ ishow-payment-service-impl/src/main/java/com/ishow/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package com.ishow.mapper;
+
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+import com.ishow.entity.User;
+
+/**
+ * <p>
+  *  Mapper 接口
+ * </p>
+ *
+ * @author cike
+ * @since 2017-09-06
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
---
+++ ishow-payment-service/src/main/java/com/ishow/service/IUserService.java
@@ -0,0 +1,16 @@
+package com.ishow.service;
+
+import com.baomidou.mybatisplus.service.IService;
+import com.ishow.entity.User;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author cike
+ * @since 2017-09-06
+ */
+public interface IUserService extends IService<User> {
+	
+}
